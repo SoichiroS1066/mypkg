14360e00cd46546a20ef1d46c2b3401ea5e0e31a
Soichiro suzuki <so11suzu@outlook.jp>
2022-12-16 14:04:01 +0900

    update mypkg

---
+++ mypkg/listener.py
@@ -0,0 +1,14 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Int16
+import sys
+print(sys.version)
+def cb(msg):
+    global node
+    node.get_logger().info("Listen: %s" % msg.data)
+
+rclpy.init()
+node = Node("listener")
+pub = node.create_subscription(Int16, "countup", cb, 10)
+
+rclpy.spin(node)
