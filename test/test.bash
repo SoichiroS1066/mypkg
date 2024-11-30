a351359f228216d5167ccb5a8ea19077dc857f25
SoichiroS1066 <so11suzu@outlook.jp>
2024-11-30 16:03:30 +0900

    test.bashの修正

--- test/test.bash
+++ test/test.bash
@@ -5,11 +5,10 @@
 dir=~
 [ "$1" != "" ] && dir="$1"   # 引数があったら、そちらをホームに変える。
 
+source $dir/.bashrc
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
 timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
-cat /tmp/mypkg.log |
-grep 'Listen: 10'
-
+cat /tmp/mypkg.log | grep 'Listen: 10'
