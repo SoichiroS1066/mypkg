26bc4574f702a3db5cbc152f93566c9372c65191
SoichiroS1066 <so11suzu@outlook.jp>
2024-12-05 13:58:42 +0900

    test.bash修正

--- test/test.bash
+++ test/test.bash
@@ -3,13 +3,19 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 dir=~
-[ "$1" != "" ] && dir="$1"   # 引数があったら、そちらをホームに変える。
+[ "$1" != "" ] && dir="$1"
 
+echo "Loading .bashrc from $dir"
 source $dir/.bashrc
+echo "Changing directory to $dir/ros2_ws"
 cd $dir/ros2_ws
+echo "Building workspace with colcon"
 colcon build
+echo "Sourcing .bashrc again"
 source $dir/.bashrc
+echo "Running ROS 2 launch"
 timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
+echo "Checking logs for 'Listen: 10'"
 cat /tmp/mypkg.log | grep 'Listen: 10'
 
