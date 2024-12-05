4fbc94689fe7cdbc3b922112e6ebc5463a194355
SoichiroS1066 <so11suzu@outlook.jp>
2024-12-05 16:55:08 +0900

    listener.pyの修正

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,21 +1,20 @@
-import rclpy  # ROS 2のクライアントのためのライブラリ
-from rclpy.node import Node  # ノードを実装するためのNodeクラス
-from std_msgs.msg import Int16  # 通信の型（16ビットの符号付き整数）
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Int16
 
-class Listener(Node):  # Nodeというクラスの機能を受け継いだクラスになる
+class Listener(Node):
     def __init__(self):
-        super().__init__("listener")  # Nodeのオブジェクトとしての初期化
+        super().__init__("listener")
         self.sub = self.create_subscription(Int16, "countup", self.cb, 10)
-        # ↑ サブスクリプションを作成。トピック "countup" を購読。
-        #    コールバック関数 `cb` を指定。
-        self.sub  # サブスクリプションを保持（ガベージコレクション防止）
 
-    def cb(self, msg):  # メッセージを受け取ったときに呼ばれるコールバック
-        self.get_logger().info(f"Listen: {msg.data}")  # 受信したデータをログに出力
+        self.sub
+
+    def cb(self, msg):
+        self.get_logger().info(f"Listen: {msg.data}")
 
 
 def main():
     rclpy.init()
-    node = Listener()  # Listenerのオブジェクトを作成
-    rclpy.spin(node)  # ↑あとは__init__が呼ばれてすべてが動き出す
+    node = Listener()
+    rclpy.spin(node)
 
