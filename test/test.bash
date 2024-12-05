72aaebe180e120588a5e1ab2e06603c666cf9dda
SoichiroS1066 <so11suzu@outlook.jp>
2024-12-05 14:14:16 +0900

    test.bash修正

--- test/test.bash
+++ test/test.bash
@@ -5,17 +5,10 @@
 dir=~
 [ "$1" != "" ] && dir="$1"
 
-echo "Loading .bashrc from $dir"
-source $dir/.bashrc
-echo "Changing directory to $dir/ros2_ws"
-cd $dir/ros2_ws
-echo "Building workspace with colcon"
+cd /root/ros2_ws  # 修正: 正しいパスに変更
 colcon build
-echo "Sourcing .bashrc again"
-source $dir/.bashrc
-echo "Running ROS 2 launch"
+source /root/.bashrc  # 修正: 正しいパスに変更
 timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
-echo "Checking logs for 'Listen: 10'"
 cat /tmp/mypkg.log | grep 'Listen: 10'
 
