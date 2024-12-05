395adb7de29a6282b6b20b681ea15fa2e5946332
SoichiroS1066 <so11suzu@outlook.jp>
2024-12-05 16:36:52 +0900

    talker.pyを書き換え

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,20 +1,26 @@
-import rclpy  #ROS 2のクライアントのためのライブラリ
-from rclpy.node import Node  #ノードを実装するためのNodeクラス（クラスは第10回で）
-from std_msgs.msg import Int16  #通信の型（16ビットの符号付き整数）
+import rclpy  # ROS 2のクライアントのためのライブラリ
+from rclpy.node import Node  # ノードを実装するためのNodeクラス（クラスは第10回で）
+from std_msgs.msg import Int16  # 通信の型（16ビットの符号付き整数）
 
-rclpy.init()
-node = Node("talker")  #ノード作成（nodeという「オブジェクト」を作成）
-pub = node.create_publisher(Int16, "countup", 10)  #パブリッシャのオブジェクト作成
-n = 0  #カウント用変数
+### ヘッダの下にTalkerというクラスを作成 ###
+class Talker(Node):  # Nodeというクラスの機能を受け継いだクラスになる
+    def __init__(self):  # オブジェクトができたときに呼ばれる
+        super().__init__("takler")  # Nodeのオブジェクトとしての初期化
+        self.pub = self.create_publisher(Int16, "countup", 10)
+        self.create_timer(0.5, self.cb)
+        self.n = 0
+        # ↑ selfはオブジェクト自身のこと
+        # ↑ オブジェクトにひとつパブリッシャと変数をもたせる。
 
-def cb():  #20行目で定期実行されるコールバック関数
-    global n  #関数を抜けてもnがリセットされないようにしている
-    msg = Int16()  #メッセージの「オブジェクト」
-    msg.data = n  #msgオブジェクトの持つdataにnを結び付け
-    pub.publish(msg)  #pubの持つpublishでメッセージ送信
-    n += 1
+    def cb(self):  # コールバックのメソッド
+        msg = Int16()
+        msg.data = self.n  # 属性には必ずselfをつける
+        self.pub.publish(msg)
+        self.n += 1
+
 
 def main():
-    node.create_timer(0.5, cb)  #タイマー設定
-    rclpy.spin(node)  #実行（無限ループ）
+    rclpy.init()
+    node = Talker()  # Talkerのオブジェクトを作成
+    rclpy.spin(node)  # ↑あとは__init__が呼ばれてすべてが動き出す
 
