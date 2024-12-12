9e00d231ce9ba41b14a28320eabf821968512574
SoichiroS1066 <so11suzu@outlook.jp>
2024-12-12 17:00:29 +0900

    課題用の加筆

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
-import os                   #追加。OSの機能のパッケージ
-from glob import glob       #追加。グロブ（ワイルドカード）を扱う関数
+import os
+from glob import glob
 
 package_name = 'mypkg'
 
@@ -11,8 +11,8 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py'))
-    ],                          #↑追加
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py'))  # launch/*.pyを追加
+    ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Soichiro Suzuki',
@@ -22,8 +22,10 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'talker = mypkg.talker:main',
-            'listener = mypkg.listener:main',
+            'talker = mypkg.talker:main',  # 既存のエントリポイント
+            'listener = mypkg.listener:main',  # 既存のエントリポイント
+            'declare_number_pub = mypkg.declare_number_pub:main',  # 新しいエントリポイント
+            'declare_number_sub = mypkg.declare_number_sub:main',  # 新しいエントリポイント
         ],
     },
 )
