145a1ce96e18eac890dfcbeb76fe4e97620258a7
SoichiroS1066 <so11suzu@outlook.jp>
2024-12-30 22:02:58 +0900

    Add a command

---
+++ launch/talk_listen2.launch.py
@@ -0,0 +1,30 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+
+def generate_launch_description():
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'mode', default_value='countup', description='countup or countdown'
+        ),
+        DeclareLaunchArgument(
+            'start_value', default_value='10', description='Starting value for countdown'
+        ),
+        Node(
+            package="mypkg",
+            executable="talker2",
+            name="talker_node",
+            output="screen",
+            parameters=[{
+                'mode': LaunchConfiguration('mode'),
+                'start_value': LaunchConfiguration('start_value')
+            }]
+        ),
+        Node(
+            package="mypkg",
+            executable="listener2",
+            name="listener_node"
+        )
+    ])
+
