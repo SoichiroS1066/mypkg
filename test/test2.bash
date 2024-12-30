c45d82f92917006a81066d0626849528cc080c4f
SoichiroS1066 <so11suzu@outlook.jp>
2024-12-30 23:18:52 +0900

    test2.bashの修正

--- test/test2.bash
+++ test/test2.bash
@@ -15,9 +15,19 @@ colcon build
 # Source the ROS 2 environment
 source $dir/.bashrc
 
-# Run the ROS 2 launch file with the specified parameters (countdown mode and start value)
-timeout 10 ros2 launch mypkg talk_listen2.launch.py mode:=countdown start_value:=10 > /tmp/mypkg.log
+# Run the ROS 2 launch file with the specified parameters (countup mode and start value)
+timeout 10 ros2 launch mypkg talk_listen2.launch.py mode:=countup start_value:=0 > /tmp/mypkg.log
 
-# Check the log file for the expected output (Listen: 10)
+# Check the log file for the expected output (Listen: 0, Listen: 1, etc.)
+cat /tmp/mypkg.log | grep 'Listen: 0'
+cat /tmp/mypkg.log | grep 'Listen: 1'
+cat /tmp/mypkg.log | grep 'Listen: 2'
+cat /tmp/mypkg.log | grep 'Listen: 3'
+cat /tmp/mypkg.log | grep 'Listen: 4'
+cat /tmp/mypkg.log | grep 'Listen: 5'
+cat /tmp/mypkg.log | grep 'Listen: 6'
+cat /tmp/mypkg.log | grep 'Listen: 7'
+cat /tmp/mypkg.log | grep 'Listen: 8'
+cat /tmp/mypkg.log | grep 'Listen: 9'
 cat /tmp/mypkg.log | grep 'Listen: 10'
 
