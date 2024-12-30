01fe0f096b1991ba0a59421ad3524d6142382b61
SoichiroS1066 <so11suzu@outlook.jp>
2024-12-31 04:47:57 +0900

    .launchの修正

--- launch/talk_listen2.launch.py
+++ launch/talk_listen2.launch.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+# SPDX-FileCopyrightText: 2024 Soichiro Suzuki
+# SPDX-License-Identifier: BSD-3-Clause
+
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
