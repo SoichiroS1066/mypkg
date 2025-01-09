29559e6dff2818bb6ad5ebaf1bc1e3a3a547129e
SoichiroS1066 <so11suzu@outlook.jp>
2025-01-10 07:36:46 +0900

    setup.pyの修正

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     entry_points={
         'console_scripts': [
             'skytree_weather_publisher = mypkg.skytree_weather_publisher:main',
-            'weather_outfit_advisor = mypkg.weather_outfit_advisor:main',
+            'skytree_weather_outfit_advisor = mypkg.skytree_weather_outfit_advisor:main',
         ],
     },
 )
