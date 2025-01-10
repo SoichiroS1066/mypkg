dbbd88ab5a44ad18b6b576b1ac3909a0be68824d
SoichiroS1066 <so11suzu@outlook.jp>
2025-01-10 09:39:02 +0900

    諸々の修正

--- launch/talk_listen.launch.py
+++ launch/talk_listen.launch.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# SPDX-FileCopyrightText: 2024 Soichiro Suzuki
+# SPDX-FileCopyrightText: 2025 Soichiro Suzuki
 # SPDX-License-Identifier: BSD-3-Clause
 
 from launch import LaunchDescription
--- test/test.bash
+++ test/test.bash
@@ -9,22 +9,16 @@ cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
 
-# talk_listen.launch.py の起動（タイムアウトを設定）
 timeout 30 ros2 launch mypkg talk_listen.launch.py &
 
-# パブリッシャが起動するのを待機
 sleep 10
 
-# ros2 topic echo をバックグラウンドで実行
 ros2 topic echo /weather_info | grep -m 1 '東京スカイツリーの天気情報' &
 
-# echo のプロセス ID を取得
 pid=$!
 
-# タイムアウトを設定（20秒後にプロセスがまだ動いていれば終了）
 sleep 20
 if ps -p $pid > /dev/null; then
-    # プロセスがまだ動いていれば終了
     kill $pid
     echo "OK"
 else
